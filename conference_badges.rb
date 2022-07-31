cf2d6694e8a26bc77ff7ba4826be44f6889fab8a
Dat Nguyen <dnguyenk2@gmail.com>
2022-07-31 14:15:40 -0700

    Completed Assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |n| 
+        "Hello, my name is #{n}."
+    end
+end
+
+def assign_rooms(names)
+    names.map.with_index(1) do |n, num|
+        "Hello, #{n}! You'll be assigned to room #{num}!"
+    end
+end
+
+def printer(names)
+    batch_badge_creator(names).each do |b|
+        puts b
+    end
+
+    assign_rooms(names).each do |n|
+        puts n
+    end
+end
+
